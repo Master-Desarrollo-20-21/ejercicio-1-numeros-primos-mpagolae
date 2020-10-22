f8ae4e3bdcd651db967e7eb4395f896d6bacea77
mpagola <72516648+mpagolae@users.noreply.github.com>
2020-10-22 13:51:35 +0200

    Create ejer2.java

---
+++ ejer2.java
@@ -0,0 +1,24 @@
+package escuela_it.poo;
+
+public class Primo2 {
+	public static void main(String[] args) {
+		int i=0;
+		boolean esPrimo=false;
+		int suma=0;
+		for (i=2; i <50; i++) {
+			esPrimo=true;
+			for (int j = 2; j <= i-1; j++) {
+				if (i%j==0) {
+					esPrimo=false;
+				}
+			}
+			if (esPrimo == true) {
+				suma = suma + i;
+				i++;
+			}
+		}
+
+		System.out.println("La suma de los números primos en los primeros 50 núemros es " + suma);
+	}
+
+}
